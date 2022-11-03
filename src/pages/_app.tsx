d84e1d4642a6b2370264b0a8bd7efe428eb2d8c5
prathamrpatel <rprathampatel@gmail.com>
2022-11-03 15:50:50 -0400

    init3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { PaginatedPosts } from '../generated/graphql';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://blogging-site-dfkeo4.herokuapp.com/graphql',
   credentials: 'include',
   cache: new InMemoryCache({
     typePolicies: {
